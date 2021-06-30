743b24a0f99f9a1d6c44d90c703bed755ec6dc8f
anne <annewithanme@gmail.com>
2021-07-01 00:14:41 +0900

    Add: 회원가입 축하 메일

--- users/utils.py
+++ users/utils.py
@@ -1,12 +1,11 @@
-import jwt
-import json
-import requests
-from json.decoder import JSONDecodeError
+import jwt,smtplib,ssl
+from email.message import EmailMessage
 
 from django.http import JsonResponse
+from django.template.loader import get_template
 
 from .models     import User
-from my_settings import SECRET_KEY, ALGORITHM
+from my_settings import SECRET_KEY, ALGORITHM, EMAIL_HOST,EMAIL_PASSWORD, EMAIL_PORT
 
 def authorize_user(func):
     def wrapper(self,request, **kwarg):
@@ -50,3 +49,42 @@ def sort_user(func):
         return func(self,request)
 
     return wrapper
+
+def send_mail_all(self,receiver_email,email_message):
+    try:
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com',EMAIL_PORT,context=context) as server:
+            server.login(EMAIL_HOST,EMAIL_PASSWORD)
+            server.sendmail(EMAIL_HOST,receiver_email,email_message.as_string())
+            server.quit()
+    except:
+        pass
+
+class Mail:
+    def create(self, receiver, subject, template):
+        try:
+            self.receiver = receiver
+            self.message  = EmailMessage()
+
+            self.message["Subject"] = subject
+            self.message["From"]    = EMAIL_HOST
+            self.message["To"]      = receiver
+
+            content          = get_template(template)
+            content_rendered = content.render(context={'receiver':receiver})
+            self.message.set_content((content_rendered),'html')
+            
+        except:
+            pass        
+
+    def send(self):
+        try:
+            context = ssl.create_default_context()
+
+            with smtplib.SMTP_SSL('smtp.gmail.com',EMAIL_PORT,context=context) as server:
+                server.login(EMAIL_HOST,EMAIL_PASSWORD)
+                server.sendmail(EMAIL_HOST,self.receiver, self.message.as_string())
+                server.quit()
+        except:
+            pass
