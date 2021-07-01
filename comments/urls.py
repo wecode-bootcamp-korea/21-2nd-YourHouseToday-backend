00936459371f49ee806d3fcf6df86e158c7a9731
cj4207 <cksdud1226@gmail.com>
2021-07-01 21:45:53 +0900

    Refactor : 오류 수정

--- comments/urls.py
+++ comments/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import CommentView
 
 urlpatterns = [
+    path('', CommentView.as_view()),
     path('/<int:comment_id>', CommentView.as_view()),
-    path('', CommentView.as_view())
+    
 ]
